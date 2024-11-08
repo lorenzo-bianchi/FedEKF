a2ca6f8b0884f1447d94a500d91525e4b3f99e7e
Lorenzo Bianchi <lnz.bnc@gmail.com>
2024-11-08 17:58:25 +0100

    chore: optimize ransacRototranslation

--- ransacRototranslation.m
+++ ransacRototranslation.m
@@ -1,11 +1,10 @@
 function [bestR, bestT, inliers] = ransacRototranslation(A, B, numIterations, distanceThreshold, minInliers)
-    numPoints = size(A, 1);
     bestInlierCount = 0;
+    inliers = [];
     bestR = eye(2);
     bestT = [0; 0];
-    found_candidate = false;
 
-    combinations = nchoosek(1:numPoints, 3);
+    combinations = nchoosek(1:size(A, 1), 3);
 
     for i = 1:size(combinations, 1)
         if i > numIterations
@@ -27,21 +26,13 @@ function [bestR, bestT, inliers] = ransacRototranslation(A, B, numIterations, di
         % Count inliers
         inlierCount = length(find(distances < distanceThreshold));
         
-        % Update the best transformation if current one is better
-        if inlierCount > bestInlierCount && inlierCount >= minInliers
-            found_candidate = true;
-            bestInlierCount = inlierCount;
+        if inlierCount >= minInliers
             bestR = R;
             bestT = T;
+            inliers = find(distances < distanceThreshold);
+            return
         end
     end
 
-    % Final inliers based on best transformation
-    if found_candidate
-        A_transformed = (bestR * A' + bestT)';
-        distances = sqrt(sum((A_transformed - B).^2, 2));
-        inliers = find(distances < distanceThreshold);
-    else
-        inliers = [];
-    end
+    return
 end
